485e325e2d38eb9af2f3b74898692652bcc23bf9
Dan Nusinovich <dann@Dan-Nusinovich-Mac.local>
2023-09-25 21:32:30 -0700

    worked on new data based connection

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,8 +1,14 @@
 # Import Flask
 from flask import Flask
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
+
+    # Database config
+    #need to fix the password before starting
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Password!@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
     @app.route('/')
     def hello():
@@ -15,6 +21,10 @@ def create_app():
     # register fact blueprint 
     from . import fact
     app.register_blueprint(fact.bp)
+
+    from . import modles
+    modles.db.init_app(app)
+    migrate = Migrate(app, modles.db)
     
     # return the app 
     return app
--- petfax/fact.py
+++ petfax/fact.py
@@ -1,7 +1,24 @@
-from flask import ( Blueprint, render_template ) 
+from flask import ( Blueprint, render_template, request, redirect ) 
+from . import modles
 
 bp = Blueprint('fact', __name__, url_prefix="/facts")
 
 @bp.route('/new')
 def new(): 
     return render_template('facts/new.html')
+
+@bp.route('/', methods = ['GET', 'POST'])
+def index():
+    if request.method == 'POST':     
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+
+        new_fact = modles.Fact(submitter=submitter, fact=fact)
+        modles.db.session.add(new_fact)
+        modles.db.session.commit()
+
+        return redirect('/facts')
+    
+    results = modles.Fact.query.all()
+    
+    return render_template('facts/index.html', facts = results)
